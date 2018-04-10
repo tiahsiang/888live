4826c5c9ed54cc29f162da428ac28aca586eebcf
sunflower <singsunflower@163.com>
2018-04-10 18:59:37 +0800

    add live details modal

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -29,10 +29,13 @@ $('.dialog-tab').on('click', 'li', function (e) {
 });
 
 
-/** 个人中心 通用模态框 **/
+/** 通用模态框 **/
 let button1 = $('#add-account-btn'); /* 添加收款账号 btn */
 let button2 = $('#add-gly'); /* 添加管理员 btn */
 let button3 = $('#add-black'); /* 添加封禁名单 btn */
+let button4 = $('.set-atv'); /* 设置广告位价格 btn */
+
+
 let layout = $('.modal-layout');
 let box = $('.modal-box');
 let comfirmBtn = $('.confirm-btn');
@@ -50,7 +53,10 @@ button3.on('click', function () {
   layout.css('display', 'block');
   box.css('display', 'block');
 });
-
+button4.on('click', function () {
+  layout.css('display', 'block');
+  box.css('display', 'block');
+});
 hide.on('click', function () {
   layout.css('display', 'none');
   box.css('display', 'none');
